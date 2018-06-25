e827e36308d12c0b8aab7a5d97e70ca8c4d49988
Thanh Do <dlnthanh@DLNTHANH.alcatel-lucent.com>
2018-06-25 18:48:44 +0700

    Nhat's contribution

---
+++ src/com/ozone/robocode/team/MyRobot.java
@@ -0,0 +1,128 @@
+package com.ozone.robocode.team;
+
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+import java.awt.Color;
+import java.io.IOException;
+
+import com.ozone.robocode.utils.Point;
+import com.ozone.robocode.utils.RobotColors;
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.ScannedRobotEvent;
+import robocode.StatusEvent;
+import robocode.tma.TTeamLeaderRobot;
+
+
+public class MyRobot extends TTeamLeaderRobot {
+    boolean peek;
+    double moveAmount;
+    double enemyX;
+    double enemyY;
+
+
+    public MyRobot(){
+    }
+
+    @Override
+    public void onRun() {
+        RobotColors c = new RobotColors();
+        c.bodyColor = Color.blue;
+        c.gunColor = Color.blue;
+        c.radarColor = Color.blue;
+        c.scanColor = Color.yellow;
+        c.bulletColor = Color.white;
+
+        try {
+            // Send RobotColors object to our entire team
+            broadcastMessage(c);
+        } catch (IOException ignored) {}
+        this.initialize();
+        this.moveAmount = Math.max(this.getBattleFieldWidth(), this.getBattleFieldHeight());
+        this.peek = false;
+        this.turnLeft(this.getHeading() % 90.0D);
+        this.ahead(this.moveAmount);
+        this.peek = true;
+        this.turnGunRight(90.0D);
+        this.turnRight(90.0D);
+        while(true) {
+            this.peek = true;
+            this.ahead(this.moveAmount);
+            this.peek = false;
+            this.turnLeft(180.0D);
+        }
+    }
+
+    @Override
+    public void onStatus(StatusEvent e) {
+        setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Don't fire on teammates
+        if (isTeammate(e.getName())) {
+            return;
+        }
+        double enemyBearing = this.getHeading() + e.getBearing();
+//             Calculate enemy's position
+            enemyX = getX() + e.getDistance() * Math.sin(Math.toRadians(enemyBearing));
+            enemyY = getY() + e.getDistance() * Math.cos(Math.toRadians(enemyBearing));
+
+            double dx = enemyX - this.getX();
+            double dy = enemyY - this.getY();
+            // Calculate angle to target
+            double theta = Math.toDegrees(Math.atan2(dx, dy));
+
+            // Turn gun to target
+            turnGunRight(normalRelativeAngleDegrees(theta - getGunHeading()));
+//             Fire hard!
+            if(this.getEnergy() > 50){
+                fire(3);
+            }else if(this.getEnergy() <= 50){
+                fire(0.5D);
+            }
+
+        try {
+            // Send enemy position to teammates
+            broadcastMessage(new Point(enemyX, enemyY));
+
+        } catch (IOException ex) {
+            out.println("Unable to send order: ");
+            ex.printStackTrace(out);
+        }
+    }
+
+
+    public void onHitRobot(HitRobotEvent e) {
+        if (e.getBearing() > -90.0D && e.getBearing() < 90.0D) {
+            this.back(100);
+        } else {
+            this.ahead(100);
+        }
+    }
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+        if (e.getBearing() > -90.0D && e.getBearing() < 90.0D) {
+            this.turnRight(90);
+            this.back(100.0D);
+        } else {
+            this.turnRight(90);
+            this.ahead(100.0D);
+        }
+
+        this.setMaxVelocity(100);
+    }
+
+    private void initialize() {
+        this.setBodyColor(new Color(92, 51, 23));
+        this.setGunColor(new Color(69, 139, 116));
+        this.setRadarColor(new Color(210, 105, 30));
+        this.setBulletColor(new Color(255, 211, 155));
+        this.setScanColor(new Color(202, 255, 112));
+    }
+
+
+}
---
+++ src/com/ozone/robocode/team/MyTeamate.java
@@ -0,0 +1,94 @@
+package com.ozone.robocode.team;
+
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+import com.ozone.robocode.utils.Point;
+import com.ozone.robocode.utils.RobotColors;
+
+import robocode.HitRobotEvent;
+import robocode.MessageEvent;
+import robocode.tma.TTeamMemberRobot;
+import robocode.util.Utils;
+
+public class MyTeamate extends TTeamMemberRobot {
+
+    @Override
+    public void run() {
+        out.println("MyTeamate is ready.");
+        this.ahead(200);
+        this.fire(3.0D);
+        this.turnRight(90);
+        this.ahead(200);
+        while (true){
+            this.ahead(200);
+            this.turnRight(90);
+        }
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent e) {
+        // Fire at a point
+        if (e.getMessage() instanceof Point) {
+            Point p = (Point) e.getMessage();
+            // Calculate x and y to target
+            double dx = p.getX() - this.getX();
+            double dy = p.getY() - this.getY();
+            // Calculate angle to target
+            double theta = Math.toDegrees(Math.atan2(dx, dy));
+            // Turn gun to target
+            turnGunRight(normalRelativeAngleDegrees(theta - getGunHeading()));
+
+            // Fire hard!
+            if(this.getEnergy() > 50){
+                fire(3);
+            }else if(this.getEnergy() <= 50){
+                fire(0.5D);
+            }
+        } // Set our colors
+        else if (e.getMessage() instanceof RobotColors) {
+            RobotColors c = (RobotColors) e.getMessage();
+
+            setBodyColor(c.bodyColor);
+            setGunColor(c.gunColor);
+            setRadarColor(c.radarColor);
+            setScanColor(c.scanColor);
+            setBulletColor(c.bulletColor);
+        }
+    }
+
+
+
+//
+//    @Override
+//    public void onHitByBullet(HitByBulletEvent e) {
+//        this.turnRight(Utils.normalRelativeAngleDegrees(90.0D - (this.getHeading() - e.getHeading())));
+//        this.ahead((double)this.dist);
+//        this.dist *= -1;
+//        this.scan();
+//    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent e) {
+        // If he's in front of us, set back up a bit.
+        if (e.getBearing() > -90 && e.getBearing() < 90) {
+            back(100);
+        } // else he's in back of us, so set ahead a bit.
+        else {
+            ahead(100);
+        }
+    }
+
+    private double bearingTo(double heading, double x, double y) {
+        return Utils.normalRelativeAngle(this.angleTo(x, y) - heading);
+    }
+
+    private double angleTo(double x, double y) {
+        return Math.atan2(x - this.getX(), y - this.getY());
+    }
+
+    private double distanceTo(double x, double y) {
+        return Math.hypot(x - this.getX(), y - this.getY());
+    }
+
+}
